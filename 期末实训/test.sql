48ec217eb0fe56b0e1755da7a6bd130867337830
yezhiming <969059910@qq.com>
2018-12-28 10:31:37 +0800

    bbbb

---
+++ 期末实训/test.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : air booking system
+Source Server Version : 50018
+Source Host           : localhost:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50018
+File Encoding         : 65001
+
+Date: 2018-12-18 23:41:22
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
